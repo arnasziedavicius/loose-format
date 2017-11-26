13dd2fd8fcbb6764675572713c032d965f41f933
arnasziedas <arnas.ziedavicius@hotmail.com>
2017-11-26 22:56:24 +0000

    Item styling

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="item">
-    <img :src="item.image.src" :width="item.image.width" :height="item.image.height" :alt="item.title" />
+    <img 
+      :src="item.image.src" 
+      :width="item.image.width" 
+      :height="item.image.height" 
+      :alt="item.title" 
+      class="item__image" />
     <div class="item__info">
       <h3>{{ item.name }} – {{ item.latitude }} {{ item.longitude }}</h3>
-      <div class="text" v-html="item.text"></div>
+      <div class="item__text" v-html="item.text"></div>
       <div class="item__info__bottom">
         <span class="play"><a href="#">Play</a></span>
         <span class="duration">{{ item.duration }}</span>
@@ -21,3 +26,25 @@
     }
   }
 </script>
+
+<style lang="less">
+
+  .item {
+    width: 58%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 6.2rem;
+
+    &__text {
+      margin-bottom: 1em;
+    }
+
+    &__image {
+      display: block;
+      width: 72%;
+      margin-left: auto;
+      margin-right: auto; 
+      margin-bottom: 0.5em;
+    }
+  }
+</style>
