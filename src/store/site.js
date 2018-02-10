d5fc6d2dfbd9a06c784c4b05b55264d641d4bf92
arnasziedas <arnas.ziedavicius@hotmail.com>
2018-02-10 17:09:08 +0000

    Change data

--- src/store/site.js
+++ src/store/site.js
@@ -15,7 +15,7 @@ export default {
           'width': '1280',
           'height': '1280'
         },
-        'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        'text': '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'
       },
       {
         'name': 'foolscap',
@@ -29,63 +29,7 @@ export default {
           'width': '1280',
           'height': '1280'
         },
-        'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-      },
-      {
-        'name': 'foolscap',
-        'slug': 'test01',
-        'lat': '51.539226',
-        'lng': '-0.052817',
-        'duration': '0:00:00',
-        'file': '/static/files/track-01.mp3',
-        'image': {
-          'src': '/static/images/cover-01.jpg',
-          'width': '1280',
-          'height': '1280'
-        },
-        'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-      },
-      {
-        'name': 'foolscap',
-        'slug': 'test02',
-        'lat': '51.539226',
-        'lng': '-0.052817',
-        'duration': '0:00:00',
-        'file': '/static/files/track-02.mp3',
-        'image': {
-          'src': '/static/images/cover-02.jpg',
-          'width': '1280',
-          'height': '1280'
-        },
-        'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-      },
-      {
-        'name': 'foolscap',
-        'slug': 'test01',
-        'lat': '51.539226',
-        'lng': '-0.052817',
-        'duration': '0:00:00',
-        'file': '/static/files/track-01.mp3',
-        'image': {
-          'src': '/static/images/cover-01.jpg',
-          'width': '1280',
-          'height': '1280'
-        },
-        'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-      },
-      {
-        'name': 'foolscap',
-        'slug': 'test02',
-        'lat': '51.539226',
-        'lng': '-0.052817',
-        'duration': '0:00:00',
-        'file': '/static/files/track-02.mp3',
-        'image': {
-          'src': '/static/images/cover-02.jpg',
-          'width': '1280',
-          'height': '1280'
-        },
-        'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        'text': '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut </p><p>labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'
       }
     ]
   }
