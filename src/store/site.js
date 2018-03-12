61112e39c0b46265761f94cc84afbb4a60ab372f
arnasziedas <arnas.ziedavicius@hotmail.com>
2018-03-12 21:46:14 +0000

    Move items to json

--- src/store/site.js
+++ src/store/site.js
@@ -1,36 +1,8 @@
+import items from '../../static/items'
+
 export default {
   namespaced: true,
-
   state: {
-    items: [
-      {
-        'name': 'foolscap',
-        'slug': 'test01',
-        'lat': '51.539226',
-        'lng': '-0.052817',
-        'duration': '0:00:00',
-        'file': '/static/files/track-01.mp3',
-        'image': {
-          'src': '/static/images/cover-01.jpg',
-          'width': '1280',
-          'height': '1280'
-        },
-        'text': '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'
-      },
-      {
-        'name': 'foolscap',
-        'slug': 'test02',
-        'lat': '51.539226',
-        'lng': '-0.052817',
-        'duration': '0:00:00',
-        'file': '/static/files/track-02.mp3',
-        'image': {
-          'src': '/static/images/cover-02.jpg',
-          'width': '1280',
-          'height': '1280'
-        },
-        'text': '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut </p><p>labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'
-      }
-    ]
+    items
   }
 }
