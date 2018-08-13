a33b9522699741924a6cdeccaca88c97687610ab
Arnas Ziedavicius <arnas.ziedavicius@gmail.com>
2018-08-13 22:07:58 +0100

    Update info

--- src/views/InfoView.vue
+++ src/views/InfoView.vue
@@ -3,7 +3,7 @@
     <div class="info__text fs-md">
       <p>City light inspired mix series covering widely sourced, forward-looking music. Timeless classics alongside fresh new sounds and a lot in-between.</p>
       <p>Left click, kick back and play; right click – save and take it with you. Check back later as we make monthly updates as well.</p>
-      <p>Email: <a href="mailto:hola@looseformat.com">hola@looseformat.com</a></p>
+      <p><a href="mailto:hola@looseformat.com">Email</a><br /><a href="https://soundcloud.com/looseformat" target="_blank">Soundcloud</a><br /><a href="https://www.instagram.com/looseformat/" target="_blank">Instagram</a></p>
     </div>
     <img src="/static/images/boy.png" alt="Loose Format" class="info__image" />
   </div>
